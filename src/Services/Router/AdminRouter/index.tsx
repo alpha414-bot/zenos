2ed02670f9eeaf01f1ecf82e65048372c255f7f1
Wilson PC <wilsonsamueli33@gmail.com>
2024-12-31 00:00:17 +0100

    complete merging sync

--- src/Services/Router/AdminRouter/index.tsx
+++ src/Services/Router/AdminRouter/index.tsx
@@ -1,13 +1,12 @@
 // Route list for accounts, authentication and other related personal settings
-import AdminDashoard from "@/Pages/Admin/Dashboard";
-import ErrorPage from "@/Pages/ErrorPage";
-import Inbox from "@/Pages/Inbox";
 import AdminInbox from "@/Pages/Admin/AdminInbox";
+import AdminDashoard from "@/Pages/Admin/Dashboard";
+import AdminLogin from "@/Pages/Admin/Login";
+import ErrorPage from "@/Pages/ErrorPage";
 import { queryToRegisterUser } from "@/Services/Queries/AuthQuery";
 import { Button } from "flowbite-react";
 import { Navigate, Outlet, RouteObject } from "react-router-dom";
 import { ProtectedRoute } from "../utils";
-import AdminLogin from "@/Pages/Admin/Login";
 
 const AdminRouter: RouteObject[] = [
   // admin authenticated routes

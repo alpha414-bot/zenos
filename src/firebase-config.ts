151d41da8e77ff4f74d576410e76c65979adbf99
Wilson PC <wilsonsamueli33@gmail.com>
2025-02-05 22:26:14 +0100

    New Products added

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -3,25 +3,25 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA-w9v45sGFqymT0_3OwqExBxc4MFur0tk",
-//   authDomain: "zenos-ecommerce.firebaseapp.com",
-//   projectId: "zenos-ecommerce",
-//   storageBucket: "zenos-ecommerce.firebasestorage.app",
-//   messagingSenderId: "354715463819",
-//   appId: "1:354715463819:web:ccb15c2a017599a21de48e",
-//   measurementId: "G-V1XNM8WP44",
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCIdRVESOrRuy-MkOawsemsZqcYw5TvQSQ",
-  authDomain: "vint-ecommerce.firebaseapp.com",
-  databaseURL: "https://vint-ecommerce-default-rtdb.firebaseio.com",
-  projectId: "vint-ecommerce",
-  storageBucket: "vint-ecommerce.firebasestorage.app",
-  messagingSenderId: "875924243691",
-  appId: "1:875924243691:web:39c861280476191c30eaae",
-  measurementId: "G-GW51SXRFJ3"
+  apiKey: "AIzaSyA-w9v45sGFqymT0_3OwqExBxc4MFur0tk",
+  authDomain: "zenos-ecommerce.firebaseapp.com",
+  projectId: "zenos-ecommerce",
+  storageBucket: "zenos-ecommerce.firebasestorage.app",
+  messagingSenderId: "354715463819",
+  appId: "1:354715463819:web:ccb15c2a017599a21de48e",
+  measurementId: "G-V1XNM8WP44",
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCIdRVESOrRuy-MkOawsemsZqcYw5TvQSQ",
+//   authDomain: "vint-ecommerce.firebaseapp.com",
+//   databaseURL: "https://vint-ecommerce-default-rtdb.firebaseio.com",
+//   projectId: "vint-ecommerce",
+//   storageBucket: "vint-ecommerce.firebasestorage.app",
+//   messagingSenderId: "875924243691",
+//   appId: "1:875924243691:web:39c861280476191c30eaae",
+//   measurementId: "G-GW51SXRFJ3"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
